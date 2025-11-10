488bdd2a88b8301fc933d460f6dd263752a85027
a12cca <obajiderotilu@gmail.com>
2025-11-10 23:40:39 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // Your repo path under a user/organization site is the base
-  // e.g., https://a12cca.github.io/cca/  -> base must be "/cca/"
-  base: "/cca/"
-});
